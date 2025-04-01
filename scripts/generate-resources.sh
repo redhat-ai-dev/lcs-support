c45eec5fbb339dddb7a193bb4d1180b976e02248
Jordan Dubrick <jdubrick@redhat.com>
2025-04-01 16:43:31 -0400

    fix typo in logging
    Signed-off-by: Jordan Dubrick <jdubrick@redhat.com>

--- scripts/generate-resources.sh
+++ scripts/generate-resources.sh
@@ -3,6 +3,6 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
 ROOTDIR=$(realpath $SCRIPTDIR/..)
 
-echo "Copying files from $ROOTDIR/resources to $ROOTDIR/templates/skeleton ..."
+echo "Copying files from $ROOTDIR/templates/skeleton/ to $ROOTDIR/resources ..."
 mkdir -p "$ROOTDIR"/resources && cp "$ROOTDIR"/templates/skeleton/* "$ROOTDIR"/resources/
 echo "DONE."
