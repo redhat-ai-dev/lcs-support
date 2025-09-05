0ab52bdbd2366fe2ea36b32f45070a8fcedfdb0c
Jordan Dubrick <jdubrick@redhat.com>
2025-09-05 15:43:27 -0400

    add generate env file
    Signed-off-by: Jordan Dubrick <jdubrick@redhat.com>

---
+++ scripts/generate-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
+ROOTDIR=$(realpath $SCRIPTDIR/..)
+
+echo "Creating copy of environment files in $ROOTDIR/env ..."
+cp "$ROOTDIR/env/default-values" "$ROOTDIR/env/values"
+cp "$ROOTDIR/env/default-harvester-values" "$ROOTDIR/env/harvester-values"
+echo "Files generated ..."
